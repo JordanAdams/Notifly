e9158799e660d352a0ed34ecb235a512afe12bc3
Jordan Adams <jordan_callum@live.co.uk>
2011-07-29 20:19:57 +0100

    Clean new layout and design for notifications.

--- jquery.notifly.js
+++ jquery.notifly.js
@@ -4,18 +4,6 @@
  * Licensed under the MIT license.
  *   http://www.opensource.org/licenses/mit-license.php
  *
- *
- * Notifly is a single function plugin which allows the quick display
- * of a customised notification.
- *
- * GitHub Repository: https://github.com/JordanAdams/Notifly
- *
- * Changelog:
- *     1.0 - First realease
- *
- * Support:
- *     - jQuery versions 1.4+
- *     - Tested and working in chrome 10, ie7, safari 5, opera 10.
 */
 
 (function($){
@@ -42,31 +30,31 @@
 			
 		//Add Notifly Div
 		$("body").append(
-			'<div id="notifly">' +
-				'<div id="notiflyHeader"></div>' +
-				'<div id="notiflyContent"></div>' +
+			'<div class="notifly">' +
+				'<div class="notiflyHeader"></div>' +
+				'<div class="notiflyContent"></div>' +
 			'</div>'
 		);
 			
 		//Assign the default "corner" class
-		$("#notifly").addClass("corner");
+		$(".notifly").addClass("corner");
 			
-		//Apply header to #notiflyHeader
-		$("#notiflyHeader").html(settings.header);
+		//Apply header to .notiflyHeader
+		$(".notiflyHeader").html(settings.header);
 		
-		//Apply content to #notiflyContent
-		$("#notiflyContent").html(settings.content);
+		//Apply content to .notiflyContent
+		$(".notiflyContent").html(settings.content);
 		
 		//fadeIn notifly (inout determines length)
-		$("#notifly").fadeIn(settings.inout);
+		$(".notifly").fadeIn(settings.inout);
 		
 		//Delay notifly (delay determines length)
-		$("#notifly").delay(settings.delay);
+		$(".notifly").delay(settings.delay);
 		
 		//fadeOut notifly (inout determines length)
-		$("#notifly").fadeOut(settings.inout, function() {
+		$(".notifly").fadeOut(settings.inout, function() {
 			
-			$("#notifly").detach();
+			$(".notifly").detach();
 			
 		});
 			
